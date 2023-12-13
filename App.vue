b16826cf84183e3f23e8be6267e1e1256f65d9ac
smilv <owou@icloud.com>
2023-12-13 17:51:03 +0800

    uni-btn

--- App.vue
+++ App.vue
@@ -17,17 +17,14 @@
 	/*每个页面公共css */
 	@import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
-	@import '@/static/uni.css';
 	@import '@/static/iconfont.css';
+	@import '@/static/uni.css';
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
 	}
-
 	/* #endif */
-	.example-info {
-		font-size: 14px;
-		color: #333;
-		padding: 10px;
+	.uni-btn{
+		font-weight: normal;
 	}
 </style>
